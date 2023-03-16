1123d4b8c14ab2d8a1e783cc756b55408620a5f6
LucaSagau <luca8sagau@gmail.com>
2023-03-16 13:56:01 +0200

    Added UserTurtleTest page.

---
+++ src/test/java/org/fasttrackit/scenarios/UserTurtleTest.java
@@ -0,0 +1,163 @@
+package org.fasttrackit.scenarios;
+
+import org.fasttrackit.Account;
+import org.fasttrackit.body.Modal;
+import org.fasttrackit.pages.CartPage;
+import org.fasttrackit.pages.HomePage;
+import org.fasttrackit.pages.WishlistPage;
+import org.fasttrackit.products.Product;
+import org.fasttrackit.products.ProductsInCart;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class UserTurtleTest {
+
+    HomePage homePage = new HomePage();
+
+    @AfterMethod
+    public void setup() {
+        homePage.resetPage();
+        homePage.returnToHomePage();
+    }
+    @Test(testName = "Verify user turtle can add products in cart.",
+            description = "This test verify if user turtle can add products in cart.")
+    public void userTurtleCanAddProductsInCart() {
+        Account account = new Account("turtle", "choochoo");
+        homePage.clickOnTheSignInButton();
+        Modal modal = new Modal();
+        modal.typeInUserNameField(account.getUsername());
+        modal.typeInPasswordField(account.getPassword());
+        modal.clickOnTheLoginButton();
+        Product product0 = new Product("0");
+        product0.addToBasket();
+        Product product1 = new Product("1");
+        product1.addToBasket();
+        Product product2 = new Product("2");
+        product2.addToBasket();
+        homePage.clickOnTheCartIcon();
+        CartPage cartPage = new CartPage();
+        assertEquals("3", cartPage.verifyNumberOfProductsInBasket(), "Expected number of products in cart to be 3.");
+
+    }
+
+    @Test(testName = "Verify user turtle can add products in wishlist.",
+            description = "This test verify if user turtle can add products in wishlist.")
+    public void userTurtleCanAddProductsInWishlist() {
+        Account account = new Account("turtle", "choochoo");
+        homePage.clickOnTheSignInButton();
+        Modal modal = new Modal();
+        modal.typeInUserNameField(account.getUsername());
+        modal.typeInPasswordField(account.getPassword());
+        modal.clickOnTheLoginButton();
+        Product product0 = new Product("0");
+        product0.addToFavorite();
+        Product product1 = new Product("1");
+        product1.addToFavorite();
+        Product product2 = new Product("2");
+        product2.addToFavorite();
+        homePage.clickOnTheWishlistIcon();
+        WishlistPage wishlistPage = new WishlistPage();
+        assertEquals("3", wishlistPage.verifyNumberOfProductsInWishlist(), "Expected number of products in cart to be 3.");
+
+    }
+
+    @Test(testName = "Verify user turtle can increase number of products in cart.",
+            description = "This test verify if user turtle can increase number of products in cart.")
+    public void userTurtleCanIncreaseNumberOfProductsInCart() {
+        Account account = new Account("turtle", "choochoo");
+        homePage.clickOnTheSignInButton();
+        Modal modal = new Modal();
+        modal.typeInUserNameField(account.getUsername());
+        modal.typeInPasswordField(account.getPassword());
+        modal.clickOnTheLoginButton();
+        Product product3 = new Product("3");
+        product3.addToBasket();
+        Product product4 = new Product("4");
+        product4.addToBasket();
+        Product product5 = new Product("5");
+        product5.addToBasket();
+        homePage.clickOnTheCartIcon();
+        ProductsInCart productsInCart3 = new ProductsInCart("3");
+        productsInCart3.increaseNumberOfProducts();
+        ProductsInCart productsInCart4 = new ProductsInCart("4");
+        productsInCart4.increaseNumberOfProducts();
+        ProductsInCart productsInCart5 = new ProductsInCart("5");
+        productsInCart5.increaseNumberOfProducts();
+        CartPage cartPage = new CartPage();
+        assertEquals("6", cartPage.verifyNumberOfProductsInBasket(), "Expected number of products in cart to be 6.");
+    }
+
+    @Test(testName = "Verify user turtle can decrease number of products in cart.",
+            description = "This test verify if user turtle can decrease number of products in cart.")
+    public void userTurtleCanDecreaseNumberOfProductsInCart() {
+        Account account = new Account("turtle", "choochoo");
+        homePage.clickOnTheSignInButton();
+        Modal modal = new Modal();
+        modal.typeInUserNameField(account.getUsername());
+        modal.typeInPasswordField(account.getPassword());
+        modal.clickOnTheLoginButton();
+        Product product6 = new Product("6");
+        product6.addToBasket();
+        Product product7 = new Product("7");
+        product7.addToBasket();
+        homePage.clickOnTheCartIcon();
+        ProductsInCart productsInCart6 = new ProductsInCart("6");
+        productsInCart6.increaseNumberOfProducts();
+        ProductsInCart productsInCart7 = new ProductsInCart("7");
+        productsInCart7.increaseNumberOfProducts();
+        productsInCart6 = new ProductsInCart("6");
+        productsInCart6.decreaseNumberOfProducts();
+        productsInCart7 = new ProductsInCart("7");
+        productsInCart7.decreaseNumberOfProducts();
+        CartPage cartPage = new CartPage();
+        assertEquals("2", cartPage.verifyNumberOfProductsInBasket(), "Expected number of products in cart to be 2.");
+    }
+    @Test(testName = "Verify user turtle can delete products from cart.",
+            description = "This test verify if user turtle can delete products from cart.")
+    public void userTurtleCanDeleteProductsFromCart() {
+        Account account = new Account("turtle", "choochoo");
+        homePage.clickOnTheSignInButton();
+        Modal modal = new Modal();
+        modal.typeInUserNameField(account.getUsername());
+        modal.typeInPasswordField(account.getPassword());
+        modal.clickOnTheLoginButton();
+        Product product8 = new Product("8");
+        product8.addToBasket();
+        Product product9 = new Product("9");
+        product9.addToBasket();
+        homePage.clickOnTheCartIcon();
+        ProductsInCart productsInCart9 = new ProductsInCart("9");
+        productsInCart9.deleteProductFromCart();
+        ProductsInCart productsInCart8 = new ProductsInCart("8");
+        productsInCart8.deleteProductFromCart();
+        CartPage cartPage = new CartPage();
+        assertEquals("", cartPage.verifyNumberOfProductsInBasket(), "Expected number of products in cart to be 0.");
+    }
+
+    @Test(testName = "Verify user turtle can 't delete products from cart.",
+            description = "This test verify if user turtle can 't delete products from cart.")
+    public void userTurtleCantDeleteProductsFromCart() {
+        Account account = new Account("turtle", "choochoo");
+        homePage.clickOnTheSignInButton();
+        Modal modal = new Modal();
+        modal.typeInUserNameField(account.getUsername());
+        modal.typeInPasswordField(account.getPassword());
+        modal.clickOnTheLoginButton();
+        Product product8 = new Product("8");
+        product8.addToBasket();
+        Product product9 = new Product("9");
+        product9.addToBasket();
+        homePage.clickOnTheCartIcon();
+        ProductsInCart productsInCart8 = new ProductsInCart("8");
+        productsInCart8.deleteProductFromCart();
+        ProductsInCart productsInCart9 = new ProductsInCart("9");
+        productsInCart9.deleteProductFromCart();
+        CartPage cartPage = new CartPage();
+        assertEquals("", cartPage.verifyNumberOfProductsInBasket(), "Expected number of products in cart to be 0.");
+    }
+
+
+
+}
